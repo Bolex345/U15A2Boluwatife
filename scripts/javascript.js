ee966c5212e3ee27d2de5d63384c3881e12100cb
Bolex345 <1533255@stu.tmc.ac.uk>
2025-04-06 00:08:33 +0100

    Update javascript.js
    For page 7.htlm

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,6 +1,13 @@
- function sayHi() {
-
-            alert("Hi!");    
-
+<script>
+    function showConfirmation(event) {
+        event.preventDefault();
+        alert("Your information has been successfully received!");
     }
 
+    document.getElementById("schoolForm").onsubmit = showConfirmation;
+    document.getElementById("volunteerForm").onsubmit = showConfirmation;
+    document.getElementById("consentForm").onsubmit = showConfirmation;
+</script>
+
+</body>
+</html> 
